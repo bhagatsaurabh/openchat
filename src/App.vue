13d3cd0c6abdbfe01e3cdfe79dacc7fac0d454ba
saurabh-prosoft <saurabhbhagat98die@gmail.com>
2023-11-01 19:50:05 +0530

    feat: theme styles

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script setup>
-import { computed } from 'vue';
-import { RouterView, useRoute } from 'vue-router';
-
 import { usePreferencesStore } from '@/stores/preferences';
 import MediaObserver from '@/components/MediaObserver/MediaObserver.vue';
 
 const preferences = usePreferencesStore();
 
 preferences.load();
-
-const route = useRoute();
-const path = computed(() => route.path);
 </script>
 
 <template>
