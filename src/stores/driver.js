1f1b5d7ebe2e2c3286abbc218394411b3fa284a2
saurabh-prosoft <saurabhbhagat98die@gmail.com>
2023-11-04 19:57:46 +0530

    feat: db driver, prefs

--- src/stores/driver.js
+++ src/stores/driver.js
@@ -1,1 +1,13 @@
-// Driver to make communication between pinia stores and indexed-db easier
+import { usePreferencesStore } from '@/stores/preferences';
+import { getSingleton, updateSingleton } from './database';
+
+export const registerPersistence = () => {
+  const preferencesStore = usePreferencesStore();
+  preferencesStore.$subscribe(() => {
+    updateSingleton('preferences', preferencesStore.serializableState);
+  });
+};
+
+export const getPreferences = async () => {
+  return await getSingleton('preferences');
+};
