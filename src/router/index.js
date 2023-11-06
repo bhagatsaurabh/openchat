7f54fc0ee20f59ef10d4f1343ae9c847a4d44a00
saurabh-prosoft <saurabhbhagat98die@gmail.com>
2023-11-06 21:57:47 +0530

    feat: auth page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import AuthVue from '@/views/Auth/Auth.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{}]
+  routes: [
+    {
+      path: '/',
+      component: AuthVue
+    }
+  ]
 });
 
 export default router;
