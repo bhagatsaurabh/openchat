f0c35c008474bfb92faff763af6f40e8e90612bb
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2023-12-18 21:56:36 +0530

    fix: firebase config init

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDFaXBHKkxy4XgFS1gKxCios8wJ4lZirdI',
-  authDomain: 'openchat-a5cec.firebaseapp.com',
-  projectId: 'openchat-a5cec',
-  storageBucket: 'openchat-a5cec.appspot.com',
-  messagingSenderId: '560810262596',
-  appId: '1:560810262596:web:3163a79632565394743c11',
-  measurementId: 'G-7N42Q3D25Y'
-};
+import firebaseConfig from '../../key.json';
 
 export const app = initializeApp(firebaseConfig);
 export const remoteDB = getFirestore(app);
